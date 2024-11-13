369aca2b7727a9e5bd5583aee931c9beb119dbb0
Andrew540714 <andrews117007@gmail.com>
2024-11-13 12:59:22 -0800

    add

--- webapp.py
+++ webapp.py
@@ -1,15 +1,49 @@
-from flask import Flask, request, render_template, flash
-from markupsafe import Markup
-from datetime import datetime
-
 import os
-import json
+from flask import Flask, url_for, render_template, request
+from flask import redirect
+from flask import session
 
 app = Flask(__name__)
+
+app.secret_key=os.environ["SECRET_KEY"];
 
 @app.route("/")
 def render_main():
     return render_template('home.html')
+    
+@app.route('/startOver')
+def startOver():
+    session.clear() 
+    return redirect(url_for('render_main'))
+    
+@app.route('/question1')
+def renderQuestion1():
+    return render_template('question1.html')
+
+@app.route('/question2',methods=['GET','POST'])
+def renderQuestion2():
+    session["answer1"]=request.form['answer1']
+    return render_template('question2.html')
+
+@app.route('/question3',methods=['GET','POST'])
+def renderQuestion3():
+    session["answer2"]=request.form['answer2']
+    return render_template('question3.html')
+    
+@app.route('/question4',methods=['GET','POST'])
+def renderQuestion4():
+    session["answer3"]=request.form['answer3']
+    return render_template('question4.html')
+    
+@app.route('/question5',methods=['GET','POST'])
+def renderQuestion5():
+    session["answer4"]=request.form['answer4']
+    return render_template('question5.html')
+    
+@app.route('/results',methods=['GET','POST'])
+def renderResults():
+    session["answer5"]=request.form['answer5']
+    return render_template('results.html')
 
 def is_localhost():
     root_url = request.url_root
