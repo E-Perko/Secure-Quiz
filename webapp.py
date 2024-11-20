4ef680eb79f9b5792285b40d7391694e2dbea2da
Andrew540714 <andrews117007@gmail.com>
2024-11-20 13:53:23 -0800

    add

--- webapp.py
+++ webapp.py
@@ -1,11 +1,8 @@
 import os
-import datetime
+import time
 from flask import Flask, url_for, render_template, request
 from flask import redirect
 from flask import session
-
-time1 = 0
-correct = []
 
 app = Flask(__name__)
 
@@ -17,79 +14,77 @@ def render_main():
     
 @app.route('/startOver')
 def startOver():
-    session.clear()
-    correct.clear()    
+    session.clear()    
     return redirect(url_for('render_main'))
-    
     
 @app.route('/question1')
 def renderQuestion1():
-    global time1
-    time1 = datetime.datetime.now()
+    timeelapsed=0
+    session["time1"] = time.time()
     return render_template('question1.html')
 
 @app.route('/question2',methods=['GET','POST'])
 def renderQuestion2():
-    session["answer1"]=request.form['answer1']
-    global correct
-    if session["answer1"]=="4":
-        correct.append(1)
-        session["answer1"]="CORRECT"
-    else:
-        correct.append(0)
-        session["answer1"]="INCORRECT"
+    if not "answer1" in session:
+        session["answer1"]=request.form['answer1']
     return render_template('question2.html')
 
 @app.route('/question3',methods=['GET','POST'])
 def renderQuestion3():
-    session["answer2"]=request.form['answer2']
-    global correct
-    if session["answer2"]=="3":
-        correct.append(1)
-        session["answer2"]="CORRECT"
-    else:
-        correct.append(0)
-        session["answer2"]="INCORRECT"
+    if not "answer2" in session:
+        session["answer2"]=request.form['answer2']
     return render_template('question3.html')
     
 @app.route('/question4',methods=['GET','POST'])
 def renderQuestion4():
-    session["answer3"]=request.form['answer3']
-    global correct
-    if session["answer3"]=="1":
-        correct.append(1)
-        session["answer3"]="CORRECT"
-    else:
-        correct.append(0)
-        session["answer3"]="INCORRECT"
+    if not "answer3" in session:
+        session["answer3"]=request.form['answer3']
     return render_template('question4.html')
     
 @app.route('/question5',methods=['GET','POST'])
 def renderQuestion5():
-    session["answer4"]=request.form['answer4']
-    global correct
-    if session["answer4"]=="2":
-        correct.append(1)
-        session["answer4"]="CORRECT"
-    else:
-        correct.append(0)
-        session["answer4"]="INCORRECT"
+    if not "answer4" in session:
+        session["answer4"]=request.form['answer4']
     return render_template('question5.html')
     
 @app.route('/results',methods=['GET','POST'])
 def renderResults():
-    session["answer5"]=request.form['answer5']
-    global correct
+    if not "answer5" in session:
+        session["answer5"]=request.form['answer5']
+    correct=0
+    if session["answer1"]=="4":
+        correct=correct+1
+        session["answer1"]="CORRECT"
+    else:
+        session["answer1"]="INCORRECT"
+        
+    if session["answer2"]=="3":
+        correct=correct+1
+        session["answer2"]="CORRECT"
+    else:
+        session["answer2"]="INCORRECT"
+        
+    if session["answer3"]=="1":
+        correct=correct+1
+        session["answer3"]="CORRECT"
+    else:
+        session["answer3"]="INCORRECT"
+        
+    if session["answer4"]=="2":
+        correct=correct+1
+        session["answer4"]="CORRECT"
+    else:
+        session["answer4"]="INCORRECT"
+        
     if session["answer5"]=="1":
-        correct.append(1)
+        correct=correct+1
         session["answer5"]="CORRECT"
     else:
-        correct.append(0)
         session["answer5"]="INCORRECT"
-    time2 = datetime.datetime.now()
-    global time1
-    time = time2-time1
-    return render_template('results.html', time=time, correct=sum(correct)*20)
+        
+    session["time2"] = time.time()
+    timediff = session["time2"]-session["time1"]
+    return render_template('results.html', time=timediff, correct=correct*20)
 
 def is_localhost():
     root_url = request.url_root
